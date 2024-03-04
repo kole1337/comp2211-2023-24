e2560a4e1ec0a191753729024b3b58a6d6acd82c
Min Khant Thu <mkt1u22@soton.ac.uk>
2024-03-04 21:30:15 +0000

    removed duplicates

--- seg/src/main/java/module-info.java
+++ seg/src/main/java/module-info.java
@@ -10,13 +10,9 @@ module com.example.seg {
     requires eu.hansolo.tilesfx;
     requires java.logging;
     requires com.opencsv;
-<<<<<<< HEAD
+
     requires java.desktop;
     requires org.jfree.jfreechart;
-=======
-    requires org.jfree.jfreechart;
-    requires java.desktop;
->>>>>>> 3a2fd235ab0b9bd9d69be0e167a88295dc48d288
 
     opens com.application.login to javafx.fxml;
     exports com.application.login;
