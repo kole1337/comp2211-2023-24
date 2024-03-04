cae7da3feaa80441de4bfde89270db1f9a8719c0
Nikola Parushev <56691310+kole1337@users.noreply.github.com>
2024-03-04 18:34:08 +0000

    fix paths dataset
    project absolute path for the log files for the graphs of min

--- seg/src/main/java/com/application/dashboard/DatasetCreator.java
+++ seg/src/main/java/com/application/dashboard/DatasetCreator.java
@@ -19,9 +19,9 @@ public class DatasetCreator {
     LocalDateTime endTime = LocalDateTime.parse("2015-01-30 00:00:00", dateFormatter);
 
     public DatasetCreator() {
-        this.clicksCsv = "C:\\Users\\Mel\\Documents\\comp2211\\seg\\src\\main\\resources\\2_week_campaign_2\\click_log.csv";
-        this.impressionsCsv = "C:\\Users\\Mel\\Documents\\comp2211\\seg\\src\\main\\resources\\2_week_campaign_2\\impression_log.csv";
-        this.serverCsv = "C:\\Users\\Mel\\Documents\\comp2211\\seg\\src\\main\\resources\\2_week_campaign_2\\server_log.csv";
+        this.clicksCsv = "seg/src/main/resources/2_week_campaign_2/click_log.csv";
+        this.impressionsCsv = "seg/src/main/resources/2_week_campaign_2/impression_log.csv";
+        this.serverCsv = "seg/src/main/resources/2_week_campaign_2/server_log.csv";
 
     }
 
