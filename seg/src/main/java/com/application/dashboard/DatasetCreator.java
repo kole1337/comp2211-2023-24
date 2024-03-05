90f8ced8beea5b948dbd095c1bdfe851db95ba33
Min Khant Thu <mkt1u22@soton.ac.uk>
2024-03-05 14:26:53 +0000

    finished graphs.haven't tested

--- seg/src/main/java/com/application/dashboard/DatasetCreator.java
+++ seg/src/main/java/com/application/dashboard/DatasetCreator.java
@@ -6,6 +6,7 @@ import org.jfree.data.time.Hour;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.WeekFields;
@@ -41,6 +42,14 @@ public class DatasetCreator {
             return createCPCDataset(time);
         } else if (graphName.equals("CTR")){
             return createCTRDataset(time);
+        } else if (graphName.equals("CPA")){
+            return createCPADataset(time);
+        } else if (graphName.equals("CPM")){
+            return createCPMDataset(time);
+        } else if (graphName.equals("Bounce")){
+            return createBounceDataset(time);
+        } else if (graphName.equals("BounceRate")){
+            return createBounceRateDataset(time);
         }
         else {
             return null;
@@ -199,6 +208,77 @@ public class DatasetCreator {
             averageCTR.put(dateTime,averageCTR.getOrDefault(dateTime,0.0) + CPCvalue);
         }
         return averageCTR;
+    }
+    private Map<LocalDateTime, Double> createCPADataset(String time) {
+        Map<LocalDateTime, Double> totalCost = createTotalCostDataset(time);
+        Map<LocalDateTime, Double> totalAcquisitions = createConversionsDataset(time);
+        Map<LocalDateTime, Double> averageCPA = new TreeMap<>();
+        for (Map.Entry<LocalDateTime, Double> entry : totalCost.entrySet()) {
+            LocalDateTime dateTime = entry.getKey();
+            Double CPCvalue = totalCost.get(dateTime)/ totalAcquisitions.get(dateTime);
+            averageCPA.put(dateTime,averageCPA.getOrDefault(dateTime,0.0) + CPCvalue);
+        }
+        return averageCPA;
+    }
+    private Map<LocalDateTime, Double> createCPMDataset(String time) {
+        Map<LocalDateTime, Double> totalCost = createTotalCostDataset(time);
+        Map<LocalDateTime, Double> totalAcquisitions = createConversionsDataset(time);
+        Map<LocalDateTime, Double> averageCPM = new TreeMap<>();
+        for (Map.Entry<LocalDateTime, Double> entry : totalCost.entrySet()) {
+            LocalDateTime dateTime = entry.getKey();
+            Double CPCvalue = totalCost.get(dateTime)/ totalAcquisitions.get(dateTime);
+            averageCPM.put(dateTime,averageCPM.getOrDefault(dateTime,0.0) + CPCvalue);
+        }
+        return averageCPM;
+    }
+    private Map<LocalDateTime, Double> createBounceDataset(String time) {
+        Map<LocalDateTime, Double> bounceCount = new TreeMap<>();
+        try (CSVReader reader = new CSVReader(new FileReader(serverCsv))) {
+            reader.readNext();
+            List<String[]> records = reader.readAll();
+            for (String[] record : records) {
+                String enter = record[0];
+                LocalDateTime enterTime = LocalDateTime.parse(enter, dateFormatter);
+                if (enterTime.isAfter(startTime) && enterTime.isBefore(endTime)) {
+                    String leave = record[2];
+                    String pages = record[4];
+                    if(!leave.equals("n/a")){
+                        LocalDateTime leaveTime = LocalDateTime.parse(leave, dateFormatter);
+                        if(Duration.between(enterTime,leaveTime).getSeconds() > 10 && Integer.parseInt(pages) > 1){
+                            LocalDateTime roundedDate;
+                            if (time.equals("hour")) {
+                                roundedDate = enterTime.withMinute(0).withSecond(0);
+                            } else if (time.equals("day")) {
+                                roundedDate = enterTime.withHour(0).withMinute(0).withSecond(0);
+                            } else if (time.equals("week")) {
+                                int week = enterTime.get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear());
+                                roundedDate = enterTime.withHour(0).withMinute(0).withSecond(0);
+                            } else {
+                                System.out.println("Enter a valid time period");
+                                return bounceCount;
+                            }
+                            bounceCount.put(roundedDate, bounceCount.getOrDefault(roundedDate, 0.0) + 1);
+
+                        }
+                    }
+
+                }
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return bounceCount;
+    }
+    private Map<LocalDateTime, Double> createBounceRateDataset(String time) {
+        Map<LocalDateTime, Double> totalClicks = createCountByTimeDataset(clicksCsv,time);
+        Map<LocalDateTime, Double> totalBounces = createBounceDataset(time);
+        Map<LocalDateTime, Double> bounceRate = new TreeMap<>();
+        for (Map.Entry<LocalDateTime, Double> entry : totalClicks.entrySet()) {
+            LocalDateTime dateTime = entry.getKey();
+            Double CPCvalue = totalBounces.get(dateTime)/ totalClicks.get(dateTime);
+            bounceRate.put(dateTime,bounceRate.getOrDefault(dateTime,0.0) + CPCvalue);
+        }
+        return bounceRate;
     }
 
 
--- seg/src/main/java/com/application/dashboard/Graphs.java
+++ seg/src/main/java/com/application/dashboard/Graphs.java
@@ -7,58 +7,100 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.WeekFields;
 import java.util.*;
 
-import static javafx.application.Application.launch;
-
 public class Graphs {
-    com.application.dashboard.DatasetCreator dc = new com.application.dashboard.DatasetCreator();
+    private DatasetCreator dc = new DatasetCreator();
     // Define the date format used in the CSV file
-    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    LocalDateTime startTime = LocalDateTime.parse("2015-01-01 00:00:00", dateFormatter);
-    LocalDateTime endTime = LocalDateTime.parse("2015-01-30 00:00:00", dateFormatter);
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private LocalDateTime startTime = LocalDateTime.parse("2015-01-01 00:00:00", dateFormatter);
+    private LocalDateTime endTime = LocalDateTime.parse("2015-01-30 00:00:00", dateFormatter);
 
     public Graphs() {
 
     }
 
     public void createGraph(String graphName, String time) {
-        if(graphName == "CPC"){
-            createCPCGraph(time,dc.createDataset("CPC",time));
+        if (graphName.equals("TotalClicks")) {
+            createTotalClicksGraph(time, dc.createDataset("TotalClicks", time));
+        } else if (graphName.equals("TotalImpressions")) {
+            createTotalImpressionsGraph(time, dc.createDataset("TotalImpressions", time));
+        } else if (graphName.equals("TotalUniques")) {
+            createTotalUniquesGraph(time);
+        } else if (graphName.equals("Conversions")) {
+            createTotalConversionsGraph(time);
+        } else if (graphName.equals("TotalCost")) {
+            createTotalCostGraph(time);
+        } else if (graphName.equals("CPC")) {
+            createCPCGraph(time, dc.createDataset("CPC", time));
+        } else if (graphName.equals("CTR")) {
+            createCTRGraph(time, dc.createDataset("CTR", time));
+        } else if (graphName.equals("CPA")) {
+            createCPAGraph(time, dc.createDataset("CPA", time));
+        } else if (graphName.equals("CPM")) {
+            createCPMGraph(time, dc.createDataset("CPM", time));
+        } else if (graphName.equals("Bounce")) {
+            createBouncesGraph(time, dc.createDataset("Bounce", time));
+        } else if (graphName.equals("BounceRate")) {
+            createBounceRateGraph(time, dc.createDataset("BounceRate", time));
         }
-        if(graphName.equals("CTR")){
-            createCTRGraph(time,dc.createDataset("CTR",time));
-        }
-        createTotalClicksGraph(time, dc.createDataset(graphName, time));
     }
 
     private void createTotalClicksGraph(String time, Map<LocalDateTime, Double> dataset) {
-        com.application.dashboard.GraphGenerator gg = new com.application.dashboard.GraphGenerator("Clicks By " + time, "Time", "Total Clicks", dataset);
+        GraphGenerator gg = new GraphGenerator("Clicks By " + time, "Time", "Total Clicks", dataset);
         gg.generateGraph();
     }
 
     private void createTotalImpressionsGraph(String time, Map<LocalDateTime, Double> dataset) {
-        com.application.dashboard.GraphGenerator gg = new com.application.dashboard.GraphGenerator("Impressions By " + time, "Time", "Total Clicks", dataset);
+        GraphGenerator gg = new GraphGenerator("Impressions By " + time, "Time", "Total Impressions", dataset);
         gg.generateGraph();
     }
-    private void createCPCGraph(String time, Map<LocalDateTime, Double> dataset){
-        GraphGenerator gg = new GraphGenerator("CPC by" + time, "Time","Average CPC",dataset);
-    }
-    private void createCTRGraph(String time, Map<LocalDateTime, Double> dataset){
-        GraphGenerator gg = new GraphGenerator("CTR by" + time, "Time","Average CTR",dataset);
-    }
 
-    public void start() {
-        Graphs g = new Graphs();
-        g.createGraph("Conversions","hour");
-        g.createGraph("TotalClicks", "hour");
-        g.createGraph("TotalClicks", "hour");
+    private void createTotalUniquesGraph(String time) {
+        Map<LocalDateTime, Double> dataset = dc.createDataset("TotalUniques", time);
+        GraphGenerator gg = new GraphGenerator("Total Uniques By " + time, "Time", "Total Uniques", dataset);
+        gg.generateGraph();
     }
 
-   public static void main(String[] args) {
-       Graphs g = new Graphs();
-       g.createGraph("CTR","hour");
+    private void createTotalConversionsGraph(String time) {
+        Map<LocalDateTime, Double> dataset = dc.createDataset("Conversions", time);
+        GraphGenerator gg = new GraphGenerator("Conversions By " + time, "Time", "Total Conversions", dataset);
+        gg.generateGraph();
+    }
 
-   }
+    private void createTotalCostGraph(String time) {
+        Map<LocalDateTime, Double> dataset = dc.createDataset("TotalCost", time);
+        GraphGenerator gg = new GraphGenerator("Total Cost By " + time, "Time", "Total Cost", dataset);
+        gg.generateGraph();
+    }
+
+    private void createCPCGraph(String time, Map<LocalDateTime, Double> dataset) {
+        GraphGenerator gg = new GraphGenerator("CPC by " + time, "Time", "Average CPC", dataset);
+        gg.generateGraph();
+    }
+
+    private void createCTRGraph(String time, Map<LocalDateTime, Double> dataset) {
+        GraphGenerator gg = new GraphGenerator("CTR by " + time, "Time", "Average CTR", dataset);
+        gg.generateGraph();
+    }
+
+    private void createCPAGraph(String time, Map<LocalDateTime, Double> dataset) {
+        GraphGenerator gg = new GraphGenerator("CPA by " + time, "Time", "Average CPA", dataset);
+        gg.generateGraph();
+    }
+
+    private void createCPMGraph(String time, Map<LocalDateTime, Double> dataset) {
+        GraphGenerator gg = new GraphGenerator("CPM by " + time, "Time", "Average CPM", dataset);
+        gg.generateGraph();
+    }
+
+    private void createBouncesGraph(String time, Map<LocalDateTime, Double> dataset) {
+        GraphGenerator gg = new GraphGenerator("Bounces By " + time, "Time", "Total Bounces", dataset);
+        gg.generateGraph();
+    }
+
+    private void createBounceRateGraph(String time, Map<LocalDateTime, Double> dataset) {
+        GraphGenerator gg = new GraphGenerator("Bounce Rate By " + time, "Time", "Bounce Rate", dataset);
+        gg.generateGraph();
+    }
 }
