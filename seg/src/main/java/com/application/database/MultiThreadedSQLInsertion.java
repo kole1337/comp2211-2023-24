839cf945c93a7de46f1d0d676e87abfc54cb2952
Nikola Parushev <56691310+kole1337@users.noreply.github.com>
2024-03-20 02:08:59 +0000

    multi threading fixed

--- seg/src/main/java/com/application/database/MultiThreadedSQLInsertion.java
+++ seg/src/main/java/com/application/database/MultiThreadedSQLInsertion.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
 
 public class MultiThreadedSQLInsertion {
     // Database credentials
@@ -16,12 +20,23 @@ public class MultiThreadedSQLInsertion {
     private static Connection conn;
 
     // Define the number of threads
-    static final int NUM_THREADS = 10; // Adjust according to your requirements
+    static final int NUM_THREADS = 500; // Adjust according to your requirements
 
     public static void main(String[] args) throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(JDBC_URL);
+        config.setUsername(DB_USER);
+        config.setPassword(DB_PASSWORD);
+        config.setMaximumPoolSize(100);
+        config.setConnectionTimeout(300000);
+        config.setIdleTimeout(120000);
+        config.setLeakDetectionThreshold(300000);
 
-        //conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
-        String csvFilePath = "D:\\year2\\seg\\comp2211\\seg\\src\\main\\resources\\2_week_campaign_2\\impression_log.csv";
+        DataSource dataSource = new HikariDataSource(config);
+
+        conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+//        String csvFilePath ="D:\\year2\\seg\\comp2211\\seg\\src\\main\\resources\\2_week_campaign_2\\impression_log.csv";
+        String csvFilePath = "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\2month\\impression_log.csv";
         ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
@@ -29,7 +44,7 @@ public class MultiThreadedSQLInsertion {
 
             while ((line = br.readLine()) != null) {
                 // Submit tasks to the thread pool
-                executor.submit(new InsertTask(line));
+                executor.submit(new InsertTask(line, dataSource));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,18 +56,20 @@ public class MultiThreadedSQLInsertion {
 
     static class InsertTask implements Runnable {
         private final String dataLine;
+        private final DataSource dataSource;
 
-        InsertTask(String dataLine) {
+        InsertTask(String dataLine, DataSource dataSource) {
             this.dataLine = dataLine;
+            this.dataSource = dataSource;
         }
 
         @Override
         public void run() {
-            try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD)){
+            try(Connection conn = dataSource.getConnection()){
                 String insertQuery = "INSERT INTO impressionlog(date, id, gender, age, income, context, impression_cost) VALUES (?,?,?,?,?,?,?)";
 //                String insertQuery = "INSERT INTO clicklog (date, id, clickcost) VALUES (?, ?, ?)";
-                //String insertQuery = "INSERT INTO clicklog (date, id, clickcost) VALUES (?, ?, ?)";
-
+//                String insertQuery = "INSERT INTO clicklog (date, id, clickcost) VALUES (?, ?, ?)";
+//
 //                try (PreparedStatement preparedStatement = conn.prepareStatement(insertQuery)) {
 //                    // Parse dataLine and set values for the prepared statement
 //                    System.out.println("Inserting");
