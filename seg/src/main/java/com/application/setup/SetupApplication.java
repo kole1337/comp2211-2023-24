08e302b0eec42605144840fb62d2c77a95618803
kole1337 <56691310+kole1337@users.noreply.github.com>
2024-04-26 16:25:29 +0100

    setup app

---
+++ seg/src/main/java/com/application/setup/SetupApplication.java
@@ -0,0 +1,27 @@
+package com.application.setup;
+
+import com.application.database.DbConnection;
+import com.application.login.LoginApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SetupApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(SetupApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("Ad dashboard");
+
+        stage.setScene(scene);
+        stage.setMinHeight(scene.getRoot().minHeight(-1));
+        stage.setMinWidth(scene.getRoot().minWidth(-1));
+        stage.show();
+        DbConnection db = new DbConnection();
+    }
+
+    public void main(String[] args) {
+        launch();
+    }
+}
